97372dd7b80b60688b20711ad30dcc08d3be247e
mariesonko <mariesonko62@gmail.com>
2018-06-03 10:38:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = "Max!";
+var welcomeMessage = "Welcome to Flatbook, "+ currentUser;
+
+var excitedWelcomeMessage = `WELCOME TO FLATBOOK,  + ${currentUser.toUpperCase()}!`;
+
+
+const firstInitial = currentUser.charAt(0);
+var shortGreeting = `Welcome, ${firstInitial}!`
